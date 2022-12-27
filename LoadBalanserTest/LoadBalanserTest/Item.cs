ab7506d31c2a970fcf6a782b1a21a91c361505cc
Moco2669 <116367622+Moco2669@users.noreply.github.com>
2022-12-27 16:12:19 +0100

    Покушај прављења нечега

---
+++ LoadBalanserTest/LoadBalanserTest/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadBalanserTest
+{
+    public enum Code { CODE_ANALOG, CODE_DIGITAL, CODE_CUSTOM, CODE_LIMITSET, CODE_SINGLENODE, CODE_MULTIPLENODE, CODE_CONSUMER, CODE_SOURCE }
+    class Item
+    {
+        Code code { get; }
+        int value { get; }
+        public Item(Code c, int v) { code = c; value = v; }
+    }
+}
