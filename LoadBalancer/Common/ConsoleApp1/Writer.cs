0bee8a38a9f1b391c6c37fd47f045c2a029c3693
Moco2669 <116367622+Moco2669@users.noreply.github.com>
2023-01-03 22:12:08 +0100

    koko ste glupi ja neverujem

---
+++ LoadBalancer/Common/ConsoleApp1/Writer.cs
@@ -0,0 +1,28 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Writer
+    {
+        Random r;
+        public void generisiIteme(IBalancerService kanal)
+        {
+            r = new Random();
+            while (true)
+            {
+                int c = r.Next(0, 8);
+                int v = r.Next(int.MinValue, int.MaxValue);
+                Item item = new Item((Code)c, v);
+                Console.WriteLine("Poslacemo Item Code: " + item.code + " Value: " + item.value);
+                kanal.zapisiIteme(item);
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
---
+++ LoadBalancer/Common/LoadBalancer/Program.cs
@@ -0,0 +1,30 @@
+﻿using Common;
+using System;
+using System.ServiceModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadBalancer
+{
+    class Program
+    {
+        public static void Conn()
+        {
+            ServiceHost sh = new ServiceHost(typeof(LoadBalancer));
+            sh.AddServiceEndpoint(typeof(IBalancerService), new NetTcpBinding(), new Uri("net.tcp://localhost:4002/IBalancerService"));
+            sh.Open();
+            Console.WriteLine("Servis otvoren.");
+
+            Console.Read();
+            //sh.Close();
+        }
+        static void Main(string[] args)
+        {
+            Task task = Task.Factory.StartNew(() => Conn());
+            Console.Read();
+            LoadBalancer lb = new LoadBalancer();
+        }
+    }
+}
